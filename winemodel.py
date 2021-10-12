20acb933d0eeefa655115b3c2bb334a0748e599d
ABBAS HAIDER <abbass.haider12345@gmail.com>
2021-10-12 15:30:45 +0500

    Initial commit

---
+++ winemodel.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,request
+import pickle
+app = Flask(__name__)
+@app.route('/' , methods=['GET', 'POST'])
+def home():
+    if request.method =='POST':
+        model = pickle.load(open("lr_model.pkl","rb"))
+        user_input = request.form.get('size')
+        user_input1 = float(user_input)
+        pred = model.predict([[user_input1]])
+        return render_template('index.html', pred=pred)
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
